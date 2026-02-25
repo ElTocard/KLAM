7a42864ef685464b69ab937f6771bf15c60a77ae
Baptiste <baptiste.lebreton@skaald.com>
2026-02-25 09:00:08 +0100

    auto-save

--- navbar.js
+++ navbar.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const isHome =
+    window.location.pathname === "/";
+
   gsap.from(".navbar_container", {
     maxWidth: window.innerWidth + "px",
     paddingLeft: "0",
-    color: "#F9F8EE",
+    ...(isHome && { color: "#F9F8EE" }),
+    border: "0px",
     backgroundColor: "transparent",
     backdropFilter: "blur(0px)",
     duration: 0.4,
